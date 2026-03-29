f6ecb70171c360819184019726f001d4b86b600c
sophiagodoy <sophia.fg@puccampinas.edu.br>
2026-03-29 16:17:42 -0300

    tirando os comentários

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\Projeto\PI5-Turma01-Grupo01\.venv"
+set "VIRTUAL_ENV=C:\Users\sophi\PI5-Turma01-Grupo01\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
